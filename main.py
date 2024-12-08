2784696c9d89e4a9f45e925bc5224421f0cae253
aditiosudirman <aditiosudirman3@gmail.com>
2024-12-09 03:49:21 +0700

    update sqlalchemy

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pydantic import EmailStr
 import web_data
 import secrets
 from itsdangerous import URLSafeTimedSerializer
+import subprocess
 
 app = FastAPI()
 
@@ -132,4 +133,4 @@ async def logout():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
